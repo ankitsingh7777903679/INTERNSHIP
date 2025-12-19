fb8d40b946619f1c28fa51ed1e8c34ca9a68e9c1
Ankit singh <149090246+ankitsingh7777903679@users.noreply.github.com>
2025-12-19 13:28:29 +0530

    feat: implement search and filter functionality in StudentTable component

--- task_1/client/src/components/StudentTable.jsx
+++ task_1/client/src/components/StudentTable.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from 'react'
-import { Table, TableBody, TableCell, TableHead, TableHeadCell, TableRow } from "flowbite-react";
+import { Table, TableBody, TableCell, TableHead, TableHeadCell, TableRow, Select, Label, TextInput } from "flowbite-react";
 import { deleteStudent, getStudentOne, getStudents } from '../api/studentService';
 import SetStatus from './SetStatus';
 
-function StudentTable({setStudent, student, setFormData, formData, refresh}) {
-    const tableRows = ["Name", "RollNo", "Email", "Phone", "Edit", "Delete", "Status"];
+function StudentTable({ setStudent, student, setFormData, formData, refresh }) {
+    const tableRows = ["Name", "RollNo", "Email", "Phone", "Edit", "Delete", " Status"];
     const [students, setStudents] = useState([])
-
-
-
+    const [filter, setFilter] = useState('pending')
+    const [searchQuery, setSearchQuery] = useState('')
+    console.log('Filter value:', filter);
     const loadData = async () => {
         try {
             const res = await getStudents();
@@ -28,7 +28,7 @@ function StudentTable({setStudent, student, setFormData, formData, refresh}) {
         try {
             console.log('Deleting student with id:', id);
             const res = await deleteStudent(id)
-            if( res.status === true){
+            if (res.status === true) {
                 alert(res.message);
                 loadData();
             }
@@ -39,9 +39,9 @@ function StudentTable({setStudent, student, setFormData, formData, refresh}) {
 
     const editStudent = async (id) => {
         try {
-             console.log('select one student with id:', id);
+            console.log('select one student with id:', id);
             const res = await getStudentOne(id)
-            if( res.status === true){
+            if (res.status === true) {
                 setStudent(res.data);
                 // Only set the fields needed for the form
                 setFormData({
@@ -56,8 +56,50 @@ function StudentTable({setStudent, student, setFormData, formData, refresh}) {
         }
     }
 
+    const filteredStudents = students.filter((student) => {
+        
+        const matchesStatus = filter ==="showAll" || student.status === filter
+
+        const q = searchQuery.toLocaleUpperCase()
+        const matchesSearch = 
+            (student.name.toLocaleUpperCase().includes(q)) ||
+            (student.email.toLocaleUpperCase().includes(q)) ||
+            (String(student.phone).toLocaleUpperCase().includes(q)) ||
+            (String(student.rollno).toLocaleUpperCase().includes(q)) ;
+
+        return matchesStatus && matchesSearch;
+
+    }
+    );
+    console.log('Filtered Students:', filteredStudents);
+
     return (
         <div>
+            <div className='flex items-center gap-2 ps-3'>
+                <div className="max-w-md">
+                    <Label htmlFor="search" value="Search" className="mb-1 block"/>
+                    <TextInput 
+                        id="search"
+                        placeholder="Search " 
+                        value={searchQuery}
+                        onChange={(e) => setSearchQuery(e.target.value)}
+                    />
+                </div>
+                {/* filter */}
+            <div className="w-25">
+
+                <Select onChange={(e) => { setFilter(e.target.value); loadData(); console.log(filter); }} id="statusFilter" value={filter} required>
+                    <option value='pending'>Pending</option>
+                    <option value='showAll'>Show All</option>
+                    <option value='active'>Active</option>
+                    <option value='suspend'>Suspend</option>
+                    <option value='delete'>Delete</option>
+                </Select>
+            </div>
+            </div>
+            {/* search */}
+            
+
             <div className="overflow-x-auto rounded p-3">
                 <Table hoverable>
                     <TableHead>
@@ -65,18 +107,21 @@ function StudentTable({setStudent, student, setFormData, formData, refresh}) {
                             {tableRows.map((row, index) => (
                                 <TableHeadCell key={index} className='bg-gray-700 text-white'>{row}</TableHeadCell>
                             ))}
+                            {/* <TableHeadCell className='bg-gray-700 text-white'>
+                                
+                            </TableHeadCell> */}
                         </TableRow>
                     </TableHead>
                     <TableBody className="divide-y">
                         {
-                            students.map((student, index) => {
+                            filteredStudents.map((student, index) => {
                                 // console.log('Student Status:', student._id);
                                 // LOGIC: If status is 'delete'
                                 const isDeleted = student.status === 'delete';
 
                                 return (
                                     <TableRow
-                                        key={index}
+                                        key={student._id}
                                         className={`bg-white dark:border-gray-700 dark:bg-gray-800 
                         ${isDeleted ? 'bg-gray-300 opacity-60' : ''}`} // Gray out if deleted
                                     >
@@ -103,7 +148,7 @@ function StudentTable({setStudent, student, setFormData, formData, refresh}) {
                                         <TableCell>
                                             <button
                                                 onClick={() => { if (!isDeleted) { studentDelete(student._id); } }}
-                                                
+
                                                 className={`font-medium ${isDeleted ? 'text-gray-400 cursor-not-allowed' : 'text-red-600 hover:underline'}`}
                                                 disabled={isDeleted}
                                             >
