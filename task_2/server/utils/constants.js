09ad8a98716e8716884c87ec81f2f84d43b8f657
Ankit singh <149090246+ankitsingh7777903679@users.noreply.github.com>
2025-12-24 18:20:29 +0530

    feat: add constants, messages, and validation utility files for API responses and error handling

---
+++ task_2/server/utils/constants.js
@@ -0,0 +1,24 @@
+// const constants = {
+//     roles: {
+//         ADMIN: 'admin',
+//         STUDENT: 'student'
+//     },
+//     studentStatus: {
+//         PENDING: 'pending',
+//         ACTIVE: 'active',
+//         SUSPEND: 'suspend', // Ye missing tha, add kar diya
+//         DELETED: 'delete'
+//     }
+// };
+
+const HTTP_RESPONSES = {
+    SUCCESS: 200,
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    NOT_FOUND: 404,
+    INTERNAL_SERVER_ERROR: 500,
+    DUPLICATE_KEY_ERROR : 11000 // MongoDB Duplicate Error Code
+};
+
+module.exports = { HTTP_RESPONSES };
---
+++ task_2/server/utils/messages/api.js
@@ -0,0 +1,14 @@
+const msg = {
+    dimond : {
+        insert : {
+            errors : {
+                invalid_param: "Invalid parameters provided",
+                email_exists: "Dimond with this shape/color/clarity/from/to already exists",
+            },
+            success : "Dimond inserted successfully"
+        },
+        
+    }
+};
+
+module.exports = { msg };
