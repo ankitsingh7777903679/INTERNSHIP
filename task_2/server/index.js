b6848ffae48b08e5291f51c9313531b4a34613dc
Ankit singh <149090246+ankitsingh7777903679@users.noreply.github.com>
2025-12-24 18:20:03 +0530

    feat: initialize server with Express and MongoDB connection

---
+++ task_2/server/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const axios = require('axios')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const DimondRouter = require('./routers/dimondRoutes')
+require('dotenv').config()
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+// routs
+app.use('/api/dimond/', DimondRouter)
+
+// Connect to MongoDB
+try {
+    mongoose.connect(process.env.DBURL).then(()=>{
+        app.listen(process.env.PORT, ()=> {
+            console.log("Server is running on http://localhost:"+process.env.PORT);
+        })
+    })
+} catch (err) {
+    console.log("Error connecting to the database", err);
+}
