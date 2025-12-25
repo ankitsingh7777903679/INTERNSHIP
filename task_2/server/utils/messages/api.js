7891b766f5bb1a557485ceabe5d39771af3eea11
Ankit singh <149090246+ankitsingh7777903679@users.noreply.github.com>
2025-12-25 12:22:51 +0530

    fix: correct typo in error message for diamond insertion

--- task_2/server/utils/messages/api.js
+++ task_2/server/utils/messages/api.js
@@ -3,7 +3,7 @@ const msg = {
         insert : {
             errors : {
                 invalid_param: "Invalid parameters provided",
-                email_exists: "Dimond with this shape/color/clarity/from/to already exists",
+                dimond_exists: "Dimond with this shape/color/clarity/from/to already exists",
             },
             success : "Dimond inserted successfully"
         },
