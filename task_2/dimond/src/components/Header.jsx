b63a396fa5a06fc3abdc8fbd53a2fd1a2876b999
Ankit singh <149090246+ankitsingh7777903679@users.noreply.github.com>
2025-12-31 14:50:25 +0530

    refactor: reorganize Header component structure for improved readability

--- task_2/dimond/src/components/Header.jsx
+++ task_2/dimond/src/components/Header.jsx
@@ -4,25 +4,28 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <>
-    <div className="fixed w-full z-10 top-0">
-      <Navbar fluid rounded  >
-        <NavbarBrand>
-          <Link to="/" className="flex items-center">
-            <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Dimond</span>
-          </Link>
-        </NavbarBrand>
-        <NavbarToggle />
-        <NavbarCollapse>
-          <NavbarLink>
-            <Link to="/dimond">Stone Group</Link>
-          </NavbarLink>
-          <NavbarLink>
-            <Link to="/dimond-price">Add Dimond</Link>
-          </NavbarLink>
-        </NavbarCollapse>
-      </Navbar>
-    </div>
-      
+      <div className="fixed w-full z-10 top-0">
+        <Navbar fluid rounded  >
+          <NavbarBrand>
+            <Link to="/" className="flex items-center">
+              <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Dimond</span>
+            </Link>
+          </NavbarBrand>
+          <NavbarToggle />
+          <NavbarCollapse>
+            <NavbarLink>
+              <Link to="/dimond">Stone Group</Link>
+            </NavbarLink>
+            <NavbarLink>
+              <Link to="/dimond-price">Add Dimond</Link>
+            </NavbarLink>
+            <NavbarLink>
+              <Link to="/diamond-list">Inventory List</Link>
+            </NavbarLink>
+          </NavbarCollapse>
+        </Navbar>
+      </div>
+
     </>
   )
 }
