337f9c52c9947cc1846d451c14101d675b7a2432
Ankit singh <149090246+ankitsingh7777903679@users.noreply.github.com>
2026-01-09 12:09:06 +0530

    feat: create layout component for blog application with metadata and font integration

---
+++ task_4/src/app/blog/layout.jsx
@@ -0,0 +1,33 @@
+import { Geist, Geist_Mono } from "next/font/google";
+// import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "BlogPage",
+  description: "A simple blog application",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="min-h-screen bg-red-50">
+        <main className=" contsiner mx-auto px-4 py-8">
+          {children}
+        </main>
+      </div>
+      </body>
+      
+
+    </html>
+  );
+}
