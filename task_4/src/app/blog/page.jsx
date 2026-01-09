b2b9703a92e03e32a415f7b5855cf7b3afa3bffc
Ankit singh <149090246+ankitsingh7777903679@users.noreply.github.com>
2026-01-09 12:08:58 +0530

    feat: fetch and display blog posts with loading state

--- task_4/src/app/blog/page.jsx
+++ task_4/src/app/blog/page.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import Link from 'next/link'
+import React, { Suspense } from 'react'
 import Post from './components/post'
 
 export default async function page() {
-  // const data = await fetch('https://jsonplaceholder.typicode.com/posts')
-  // const post = await data.json()
+  const promise = await fetch('https://jsonplaceholder.typicode.com/posts').then((res) => res.json())
+  const slicedPromise = promise.slice(0, 10);
   return (
     <div className="p-8">
       <h1 className="text-3xl font-bold mb-6">Blog Posts</h1>
-      <div>
-        <Post  />
-      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Post promise={slicedPromise} />
+      </Suspense>
     </div>
   )
 }
