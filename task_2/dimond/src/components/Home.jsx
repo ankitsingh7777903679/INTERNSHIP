b753e4e034f0c4e9b2ec0106d0d1e4eef5379e46
Ankit singh <149090246+ankitsingh7777903679@users.noreply.github.com>
2026-01-17 22:00:20 +0530

    feat: create Home component with a title header

---
+++ task_2/dimond/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h1 className='text-white mt-20 text-center text-2xl'>HOME</h1>
+    </div>
+  )
+}
+
+export default Home
