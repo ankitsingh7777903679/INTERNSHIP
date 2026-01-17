10b4ea48a9ac95c202af933942cb2fb1ce64ecaa
Ankit singh <149090246+ankitsingh7777903679@users.noreply.github.com>
2026-01-17 21:56:55 +0530

    feat: add global interceptor for handling session expiration and redirect to login

--- task_2/dimond/src/main.jsx
+++ task_2/dimond/src/main.jsx
@@ -2,6 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import axios from '../src/api/axiosInstance.js'
+
+// Add a global interceptor to handle session expiration (401)
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      // Clear user data
+      localStorage.removeItem('token');
+      localStorage.removeItem('user');
+      // Redirect to login page
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  }
+);
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
