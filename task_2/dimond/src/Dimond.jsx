fa8f89a6d3df235355358b456dfc94c3d4baa87c
Ankit singh <149090246+ankitsingh7777903679@users.noreply.github.com>
2026-01-17 21:56:22 +0530

    feat: implement user permissions for Stone Group form visibility and enhance form layout

--- task_2/dimond/src/Dimond.jsx
+++ task_2/dimond/src/Dimond.jsx
@@ -7,6 +7,15 @@ import { validationDimondForm, validationDimondField } from "./validation/dimond
 import { toast } from "react-toastify";
 // import { set } from "mongoose";
 function Dimond() {
+    const user = JSON.parse(localStorage.getItem("user"));
+    // console.log("User:", user);
+    const userRole = user?.role || ''
+    const userPermissions = user?.permissions || [];
+    const hasStoneGroupWritePermission = userPermissions.includes('stoneGroup_write') && (userRole === 'user') || (userRole === 'admin');
+
+
+
+
     const [error, setError] = useState({})
     const [dimondId, setDimondId] = useState(null);
     const [dimondValue, setDimondValue] = useState({
@@ -50,7 +59,7 @@ function Dimond() {
         const errors = validationDimondForm(dimondValue);
         if (errors) {
             setError(errors)
-            
+
             console.log("Validation errors:", error)
             toast.error(error.shape || error.color || error.clarity || error.from || error.to || error.price);
             // alert("Validation errors. Please check the form fields.");
@@ -109,81 +118,83 @@ function Dimond() {
 
     return (
         <>
-            <div className="p-4 grid grid-cols-[30%_auto] gap-7">
-                <div className="">
-                    <h1 className="text-2xl font-bold mb-4">Stone Group</h1>
-                    {/* <ToastContainer /> */}
-                    <form onSubmit={handelFormDimond} className="flex w-full flex-col gap-4 bg-gray-500 p-4 rounded-lg">
+            <div className="p-4 grid grid-cols-[40%_auto] gap-7">
+                {hasStoneGroupWritePermission && (
+                    <div className="">
+                        <h1 className="text-2xl text-white font-bold mb-4">Stone Group</h1>
+                        {/* <ToastContainer /> */}
+                        <form onSubmit={handelFormDimond} className="flex w-full flex-col gap-4 bg-gray-500 p-4 rounded-lg">
+                            <h1 className="text-2xl text-white font-bold mb-4">Stone Group</h1>
 
-                        <div>
-                            <div className="mb-2 block">
-                                <Label htmlFor="shape">Shape</Label>
-                            </div>
-                            <Select id="shape" value={dimondValue.shape} onChange={(e) => setFormDimondValue(e)} >
-                                <option value="">Select shape</option>
-                                <option value="round">Round</option>
-                                <option value="pear">Pear</option>
-                                <option value="heart">Heart</option>
-                            </Select>
-                            {error.shape && <p className='text-red-300 text-sm mt-1'>{error.shape}</p>}
-                        </div>
-                        <div>
                             <div>
                                 <div className="mb-2 block">
-                                    <Label htmlFor="color">Color</Label>
+                                    <Label htmlFor="shape">Shape</Label>
                                 </div>
-                                <Select id="color" value={dimondValue.color} onChange={(e) => setFormDimondValue(e)} >
-                                    <option value="">Select color</option>
-                                    <option value="G">G</option>
-                                    <option value="H">H</option>
-                                    <option value="I">I</option>
-                                    <option value="J">J</option>
+                                <Select id="shape" value={dimondValue.shape} onChange={(e) => setFormDimondValue(e)} >
+                                    <option value="">Select shape</option>
+                                    <option value="round">Round</option>
+                                    <option value="pear">Pear</option>
+                                    <option value="heart">Heart</option>
                                 </Select>
-                                {error.color && <p className='text-red-300 text-sm mt-1'>{error.color}</p>}
+                                {error.shape && <p className='text-red-300 text-sm mt-1'>{error.shape}</p>}
                             </div>
                             <div>
-                                <div className="mb-2 block">
-                                    <Label htmlFor="clarity">Clarity</Label>
+                                <div>
+                                    <div className="mb-2 block">
+                                        <Label htmlFor="color">Color</Label>
+                                    </div>
+                                    <Select id="color" value={dimondValue.color} onChange={(e) => setFormDimondValue(e)} >
+                                        <option value="">Select color</option>
+                                        <option value="G">G</option>
+                                        <option value="H">H</option>
+                                        <option value="I">I</option>
+                                        <option value="J">J</option>
+                                    </Select>
+                                    {error.color && <p className='text-red-300 text-sm mt-1'>{error.color}</p>}
                                 </div>
-                                <Select id="clarity" value={dimondValue.clarity} onChange={(e) => setFormDimondValue(e)} >
-                                    <option value="">Select clarity</option>
-                                    <option value="SI1">SI1</option>
-                                    <option value="VS1">VS1</option>
-                                    <option value="VVS1">VVS1</option>
-                                    <option value="VVS2">VVS2</option>
-                                </Select>
-                                {error.clarity && <p className='text-red-300 text-sm mt-1'>{error.clarity}</p>}
-                            </div>
-                            <div>
-                                <div className="mb-2 block">
-                                    <Label htmlFor="from">From</Label>
+                                <div>
+                                    <div className="mb-2 block">
+                                        <Label htmlFor="clarity">Clarity</Label>
+                                    </div>
+                                    <Select id="clarity" value={dimondValue.clarity} onChange={(e) => setFormDimondValue(e)} >
+                                        <option value="">Select clarity</option>
+                                        <option value="SI1">SI1</option>
+                                        <option value="VS1">VS1</option>
+                                        <option value="VVS1">VVS1</option>
+                                        <option value="VVS2">VVS2</option>
+                                    </Select>
+                                    {error.clarity && <p className='text-red-300 text-sm mt-1'>{error.clarity}</p>}
                                 </div>
-                                <TextInput id="from" type="number" value={dimondValue.from} onChange={(e) => setFormDimondValue(e)} placeholder="Enter 0.19.." />
-                                {error.from && <p className='text-red-300 text-sm mt-1'>{error.from}</p>}
-                            </div>
-                            <div>
-                                <div className="mb-2 block">
-                                    <Label htmlFor="to">To</Label>
+                                <div>
+                                    <div className="mb-2 block">
+                                        <Label htmlFor="from">From</Label>
+                                    </div>
+                                    <TextInput id="from" type="number" value={dimondValue.from} onChange={(e) => setFormDimondValue(e)} placeholder="Enter 0.19.." />
+                                    {error.from && <p className='text-red-300 text-sm mt-1'>{error.from}</p>}
                                 </div>
-                                <TextInput id="to" type="number" value={dimondValue.to} onChange={(e) => setFormDimondValue(e)} placeholder="enter 0.29.." />
-                                {error.to && <p className='text-red-300 text-sm mt-1'>{error.to}</p>}
+                                <div>
+                                    <div className="mb-2 block">
+                                        <Label htmlFor="to">To</Label>
+                                    </div>
+                                    <TextInput id="to" type="number" value={dimondValue.to} onChange={(e) => setFormDimondValue(e)} placeholder="enter 0.29.." />
+                                    {error.to && <p className='text-red-300 text-sm mt-1'>{error.to}</p>}
+                                </div>
+                                <div className="mb-2 block">
+                                    <Label htmlFor="price">RAP</Label>
+                                </div>
+                                <TextInput id="price" type="number" min={0} value={dimondValue.price} onChange={(e) => setFormDimondValue(e)} placeholder="Enter price" />
+                                {error.price && <p className='text-red-300 text-sm mt-1'>{error.price}</p>}
                             </div>
-                            <div className="mb-2 block">
-                                <Label htmlFor="price">RAP</Label>
+                            <div className="flex items-center justify-center gap-4 mt-4">
+                                <Button type="submit" className="bg-black">{dimondId ? "Update" : "Add New"}</Button>
+                                {dimondId ? <Button onClick={() => cancleEdite()} type="submit" className="bg-black">Cancle</Button> : ""}
+
                             </div>
-                            <TextInput id="price" type="number" min={0} value={dimondValue.price} onChange={(e) => setFormDimondValue(e)} placeholder="Enter price" />
-                            {error.price && <p className='text-red-300 text-sm mt-1'>{error.price}</p>}
-                        </div>
-                        <div className="flex items-center justify-center gap-4 mt-4">
-                            <Button type="submit" className="bg-black">{dimondId ? "Update" : "Add New"}</Button>
-                            {dimondId ? <Button onClick={()=> cancleEdite()} type="submit" className="bg-black">Cancle</Button> : ""}
-                            
-                        </div>
 
 
-                    </form>
-                </div>
-
+                        </form>
+                    </div>
+                )}
                 <div>
                     <h1 className="text-2xl font-bold mb-4">Stone Group List</h1>
                     <StoneGroupTable dimondValue={dimondValue} setDimondValue={setDimondValue} setDimondId={setDimondId} />
